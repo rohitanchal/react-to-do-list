db4b2e5300c58984238430c84e90492c1f821b2c
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-10-20 16:17:56 +0530

    All Components Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./mycomponents/Header";
+// import ToDoItems from "./mycomponents/ToDoItems";
+import ToDoS from "./mycomponents/ToDoS";
+import Footer from "./mycomponents/Footer"; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+       <>
+        <Header title="Todos List"/>
+        {/* <ToDoItems/> */}
+        <ToDoS/>
+        <Footer/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/mycomponents/ToDoS.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ToDoS() {
+  return (
+    <div>
+      <h1>TODOS</h1>
+    </div>
+  )
+}
